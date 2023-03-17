0e6fef833310c7c585f21ff10662e872d46579b7
Jesse Putnam <66268183+jessejputnam@users.noreply.github.com>
2023-03-17 15:21:31 -0400

    Final comment and aesthetic tweaks of code base

--- putnam-summative.js
+++ putnam-summative.js
@@ -20,33 +20,35 @@ const reset_btn = document.getElementById("reset-btn");
 const history = [];
 let max_num, answer;
 
-// Preload modal for start game
+// Show modal on load for start game
 window.onload = function () {
   $("#modal").modal("show");
 };
 
-// ####################### GAME PLAY #######################
-
 // Initialize gameplay on start click
 start_btn.addEventListener("click", init);
+
+// ####################### GAME PLAY #######################
+
 // Gameplay loop on guess click
 guess_btn.addEventListener("click", game_loop);
 // Start new game on reset click
 reset_btn.addEventListener("click", init);
 
 /**
- * Gameplay loop on guess click
+ * Gameplay loop via guess click
  * @returns {void}
  */
 function game_loop() {
   // Don't allow empty input
   if (input.value === "") return;
-  // Convert input string to number
+
+  // Convert input string to number and store as guess
   const guess = +input.value;
   // Reset input value
   input.value = "";
 
-  // Check for valid guess
+  // Check for valid guess, set error message if necessary
   if (is_valid(guess, max_num, history) !== "valid") return;
 
   // Add guess to history
@@ -54,14 +56,18 @@ function game_loop() {
 
   // Win scenario
   if (guess === answer) {
+    // Show reset button
     reset_btn.classList.remove("d-none");
+    // Hide guess button
     guess_btn.classList.add("d-none");
+    // Disable input to stop further guesses
     input.disabled = true;
 
+    // Set win message to display
     return set_msg(get_msg("win", null, history));
   }
 
-  // Valid guess, but incorrect
+  // Valid guess, but incorrect, set appropriate message
   if (guess < answer) set_msg(get_msg("low"));
   if (guess > answer) set_msg(get_msg("high"));
 }
@@ -71,12 +77,20 @@ function game_loop() {
  * @returns {void}
  */
 function init() {
+  // Reset gameplay message
   set_msg("");
+  // Hide reset button if visible
   reset_btn.classList.add("d-none");
+  // Show guess button if hidden
   guess_btn.classList.remove("d-none");
+  // Re-enable the input for guesses
   input.disabled = false;
+  // Erase history of array
   history.length = 0;
+  // Prompt user for new max number
   max_num = get_input();
+  // Add formatted max number to instructions
   instructions.textContent = new Intl.NumberFormat("en-US").format(max_num);
+  // Get a new randomly generated answer
   answer = rng(max_num);
 }
